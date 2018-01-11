c1611ab49d5d44cf0696f8590358673171726587
Unknown <philippedebridiers@gmail.com>
2018-01-11 01:55:36 +0100

    Config bundles

--- src/Hydrate/ApiBundle/Controller/DefaultController.php
+++ src/Hydrate/ApiBundle/Controller/DefaultController.php
@@ -3,9 +3,15 @@
 namespace Hydrate\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Rest\View()
+     * @Rest\Get("/home")
+     */
     public function indexAction()
     {
         return $this->render('HydrateApiBundle:Default:index.html.twig');
--- src/Hydrate/ApiBundle/Controller/User/SecurityController.php
+++ src/Hydrate/ApiBundle/Controller/User/SecurityController.php
@@ -3,11 +3,25 @@
 namespace Hydrate\ApiBundle\Controller\User;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SecurityController extends Controller
 {
-    public function loginCheckAction(Request $request)
+    /**
+     * @Rest\View()
+     * @Rest\Post("/login_check")
+     */
+    public function loginAction(Request $request)
+    {
+    }
+
+    /**
+     * @Rest\View()
+     * @Rest\Get("/logout")
+     */
+    public function logoutAction(Request $request)
     {
 
     }
